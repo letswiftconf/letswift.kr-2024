469aa0f07e7795468689a0f7fd1e6934d38dacf1
Bummo Koo <ksquareatm@gmail.com>
2024-08-04 11:27:18 +0900

    Add header.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="flex flex-col space-y-8">
-    <router-view></router-view>
-    <Footer/>  
+  <div class="flex flex-col">
+    <Header class="fixed top-0 left-0 w-full h-20"/>
+    <div class="pt-28 flex flex-col space-y-8">
+      <!-- <div class="flex-grow h-16 bg-blue-500"></div> -->
+      <router-view></router-view>
+      <Footer/>  
+    </div>
   </div>
 </template>
 
 <!-- Script -->
 <script setup>
+import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header class="flex justify-center backdrop-blur-lg bg-black/50">
+        <div class="px-8 xl:px-0 max-w-[1280px] flex items-center flex-grow space-x-4">
+            <!-- Left -->
+            <router-link to="/">
+                <div class="font-black text-[2em]">
+                    <span class="text-rainbow box-shadow-xl">Let'Swift 2024</span>
+                </div>
+            </router-link>
+            <div class="grow"></div>
+            <!-- Right -->
+             <div class="flex space-x-6">
+                <h1 class="font-semibold text-[1.2em] text-white ">Sponsors</h1>
+                <h1 class="font-semibold text-[1.2em] text-white ">Venue</h1>
+                <h1 class="font-semibold text-[1.2em] text-white ">Schedule</h1>
+                <h1 class="font-semibold text-[1.2em] text-white ">Speakers</h1>
+                <h1 class="font-semibold text-[1.2em] text-white ">Organizers</h1>
+                <h1 class="font-semibold text-[1.2em] text-white ">FAQ</h1>
+                <h1 class="font-bold text-[1.3em] text-white ">Get Ticket</h1>
+             </div>
+        </div>
+    </header>
+</template>
